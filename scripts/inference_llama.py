2704d2c8fd1c601dc4709cc49f351ce1909e01a8
Kevin Song <song_zj@hotmail.com>
2025-06-28 10:35:15 -0400

    output json

--- scripts/inference_llama.py
+++ scripts/inference_llama.py
@@ -8,8 +8,8 @@ hf_token = os.getenv("HF_TOKEN")
 os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3,4,5,6,7"
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
 
-# model_id = "meta-llama/Llama-3.1-8B-Instruct"
-model_id = "meta-llama/Llama-3.3-70B-Instruct"
+model_id = "meta-llama/Llama-3.1-8B-Instruct"
+# model_id = "meta-llama/Llama-3.3-70B-Instruct"
 
 begin = time.time()
 start_time = time.time()
@@ -42,33 +42,39 @@ prompt_path = "prompts/PURM25.txt"
 with open(prompt_path, "r") as f:
     system_prompt = f.read()
 
-user_message = "\"Mom reports that she has to return to work on Monday but in need of childcare. She may have to leave child with elderly GM with 16 and 10 year old in the home. In treatment for opioid use disorder, currently on methadone. Mother shared CPS involvement due to recent childline report indicating sexual abuse from father.\""
+user_message = '"Mom reports that she has to return to work on Monday but in need of childcare. She may have to leave child with elderly GM with 16 and 10 year old in the home. In treatment for opioid use disorder, currently on methadone. Mother shared CPS involvement due to recent childline report indicating sexual abuse from father."'
 
 # result = pipe(system_prompt + "\n\n" + user_message)
 # print(result[0]["generated_text"])
 
+messages = [
+    {"role": "system", "content": system_prompt},
+    {"role": "user", "content": user_message},
+]
+
 print("Tokenizing...")
-inputs = tokenizer(system_prompt + "\n\n" + user_message, return_tensors="pt")
+# inputs = tokenizer(system_prompt + user_message, return_tensors="pt")
+inputs = tokenizer.apply_chat_template(
+    messages, return_tensors="pt", add_generation_prompt=True
+)
 
 if cuda_available:
-    inputs = {k: v.to("cuda") for k, v in inputs.items()}
+    inputs = inputs.to("cuda")
 
 print(f"Tokenized in {time.time() - start_time} seconds\n\n")
 start_time = time.time()
 
 print("Generating...")
-outputs = model.generate(**inputs, max_new_tokens=512)
+outputs = model.generate(
+    **inputs, max_new_tokens=256, do_sample=False, response_format="json_object"
+)
 
 print(f"Generated in {time.time() - start_time} seconds\n\n")
 start_time = time.time()
 
 print("Decoding...")
-input_length = inputs["input_ids"].shape[1]
-generated_tokens = outputs[0][input_length:]
 
-print(tokenizer.decode(outputs[0][:input_length], skip_special_tokens=True))
-
-decoded_output = tokenizer.decode(generated_tokens, skip_special_tokens=True)
+decoded_output = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
 with open("output/test/llama_output.txt", "w") as f:
     f.write(decoded_output)
