faf4833ae97944d61ddeff4b77b8c6e61817eb39
Song <songk1@reslnvhpc0132.research.chop.edu>
2025-07-15 13:04:16 -0400

    first results

--- evaluation.py
+++ evaluation.py
@@ -2,29 +2,34 @@ import json
 from dataloader import get_dataloaders
 from load_model import load_model_pipeline
 from metrics import compute_metrics_per_label
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
 
-pipeline = load_model_pipeline("meta-llama/Llama-3.3-70B-Instruct")
+#pipeline = load_model_pipeline("meta-llama/Llama-3.3-70B-Instruct")
+model_id = "meta-llama/Llama-3.3-70B-Instruct"
+quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+quantized_model = AutoModelForCausalLM.from_pretrained(
+    model_id, device_map="auto", torch_dtype=torch.bfloat16)
+quantized_model.compile()
+
+
 prompt_path = "prompts/prompt2.txt"
 
 with open(prompt_path, "r") as f:
     system_prompt = f.read()
 
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+
+
 def generate_output(user_message, print_output=False):
-    messages = [
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": user_message},
-    ]
+    input_text = system_prompt + "\n\n" + user_messsage
+    input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
 
-    outputs = pipeline(
-        messages,
-        max_new_tokens=256,
-    )
+    output = quantized_model.generate(**input_ids, max_new_tokens=96)
 
-    if print_output:
-        print(outputs[0]["generated_text"][-1])
-
-    return outputs[0]["generated_text"][-1]
+    return tokenizer.decode(output[0], skip_special_tokens=True).cpu()
 
 def parse_output(pred):
     if pred.strip() == "NoSocialNeedsFoundLabel":
