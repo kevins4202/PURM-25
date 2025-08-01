bd96317dbbcb5dabd3a44bde718c3bc8abd38dff
Song <songk1@reslnvhpc0132.research.chop.edu>
2025-08-01 09:24:03 -0400

    evaluation try

--- evaluation.py
+++ evaluation.py
@@ -45,9 +45,12 @@ class ModelEvaluator:
             quantization_config=quantization_config,
             device_map=device,
         )
+        print("Loaded model")
         model.generation_config.pad_token_id = model.generation_config.eos_token_id[0]
         tokenizer = AutoTokenizer.from_pretrained(self.model_id)
         tokenizer.pad_token = tokenizer.eos_token
+        
+        print("Loaded tokenizer")
         
         if self.evaluation_config["eval_type"] == "s":
             self.outlined_model = outlines.from_transformers(model, tokenizer)
@@ -119,65 +122,74 @@ class ModelEvaluator:
                     # print(f"Parsing found JSON text {i}:", json_text.replace('\n', ''))
                     
                     # Try to parse as JSON
-                    parsed_json = json.loads(json_text)
-                    # print(f"Successfully parsed JSON {i}")
-                    preds.append(parsed_json)
+                    try:
+                        parsed_json = json.loads(json_text)
+                        preds.append(parsed_json)
+                    except Exception as e:
+                        print(f"Failed to parse JSON {i}")
+                        preds.append(None)
+
+                    
                 else:
                     print(f"No JSON content found for output {i}")
                     preds.append(None)
                     
         except Exception as e:
             print(f"Failed to generate batch output: {e}")
-            preds = [None] * len(notes)
+            return preds + [None] * (self.batch_size - len(preds))
                 
         return preds
     
     def evaluate(self, dataloader):
         """Evaluate model on a batch of data"""
-        preds = []
-        targets = []
-        broken_indices = []
+        try:
+            preds = []
+            targets = []
+            broken_indices = []
 
-        for idx, batch in enumerate(dataloader):
-            if (
-                self.max_batches
-                and idx >= self.max_batches
-            ):
-                break
+            for idx, batch in enumerate(dataloader):
+                if (
+                    self.max_batches
+                    and idx >= self.max_batches
+                ):
+                    break
 
-            print(f"\nbatch {idx + 1} of {self.max_batches if self.max_batches else len(dataloader)}")
-            notes, labels = batch["note"], batch["labels"]
+                print(f"\nbatch {idx + 1} of {self.max_batches if self.max_batches else len(dataloader)}")
+                notes, labels = batch["note"], batch["labels"]
 
-            if self.evaluation_config["eval_type"] == 's':
-                # Process batch
-                batch_preds = []
-                for note in notes:
-                    batch_preds.append(self.generate_structured_output(note))
-            elif self.evaluation_config["eval_type"] == 'us':
-                # Process batch
-                batch_preds = self.generate_unstructured_output(notes)
-            else:
-                raise ValueError("Invalid eval type")
+                if self.evaluation_config["eval_type"] == 's':
+                    # Process batch
+                    batch_preds = []
+                    for note in notes:
+                        batch_preds.append(self.generate_structured_output(note))
+                elif self.evaluation_config["eval_type"] == 'us':
+                    # Process batch
+                    batch_preds = self.generate_unstructured_output(notes)
+                else:
+                    raise ValueError("Invalid eval type")
             
-            # Process each prediction in the batch
-            for i, (pred, label) in enumerate(zip(batch_preds, labels)):
-                print(f"\nNOTE {i}: {notes[i][:50].replace(chr(10), '')}...\n")
-                try:
-                    if pred is None:
-                        print(f"Failed to generate output for sample {idx}_{i}")
-                        broken_indices.append(f"{idx}_{i}")
-                        continue
+                # Process each prediction in the batch
+                for i, (pred, label) in enumerate(zip(batch_preds, labels)):
+                    print(f"\nNOTE {i}: {notes[i][:50].replace(chr(10), '')}...\n")
+                    try:
+                        if pred is None:
+                            print(f"Failed to generate output for sample {idx}_{i}")
+                            broken_indices.append(f"{idx}_{i}")
+                            continue
                    
-                    print(pred)
+                        print(pred)
                     
-                    pred_annotations = get_annotations(pred)
-                    preds.append(pred_annotations)
-                    targets.append(label)
-                    print(f"\nprediction: {pred_annotations} \ntarget: {label.tolist()}")
-                except Exception as e:
-                    print(f"Error getting annotations: {e} {pred}")
-                    broken_indices.append(f"{idx}_{i}")
-
+                        pred_annotations = get_annotations(pred)
+                        preds.append(pred_annotations)
+                        targets.append(label)
+                        print(f"\nprediction: {pred_annotations} \ntarget: {label.tolist()}")
+                    except Exception as e:
+                        print(f"Error getting annotations: {e} {pred}")
+                        broken_indices.append(f"{idx}_{i}")
+        except Exception as e:
+            print("method failed, returning early")
+            return preds, targets, broken_indices
+            
         return preds, targets, broken_indices
 
     def compute_and_save_metrics(self, preds, targets, broken_indices):
@@ -206,16 +218,20 @@ class ModelEvaluator:
 
         # Compute per-label metrics
         metrics = compute_metrics(preds, targets)
-        # print("\nMetrics:", metrics)
+        print("\nMetrics:", metrics)
 
         os.makedirs("results", exist_ok=True)
         os.makedirs(f"results/{self.output_dir}", exist_ok=True)
+        
+        file_name = self.prompt_path.split('_')[0] + "_" + str(1 - int(self.evaluation_config["zero_shot"]))+ "_shot"
 
         i = 0
-        while os.path.exists(f"results/{self.output_dir}/{self.prompt_path}_{i}.json"):
+        while os.path.exists(f"results/{self.output_dir}/{file_name}_{i}.json"):
             i += 1
-
-        with open(f"results/{self.output_dir}/{self.prompt_path}_{i}.json", "w") as f:
+        
+        print(f"\n\nSaving metrics to: results/{self.output_dir}/{file_name}_{i}.json") 
+            
+        with open(f"results/{self.output_dir}/{file_name}_{i}.json", "w") as f:
             json.dump(metrics, f, indent=2)
 
 def main():
@@ -239,7 +255,7 @@ def main():
     parser.add_argument('--few-shot', action='store_true', help='Use few-shot evaluation (default if neither specified)')
     parser.add_argument('--model-id', type=str, required=True, help='Model ID (overwrites config)')
     parser.add_argument('--eval-type', type=str, required=True, help='Evaluation type (structured or unstructured or vllm)')
-    parser.add_argument('--max-batches', type=int, default=1, help='Maximum number of batches to process (default: 5)')
+    parser.add_argument('--max-batches', type=int, default=None, help='Maximum number of batches to process (default: 5)')
     parser.add_argument('--batch-size', type=int, default=16, help='Batch size for processing (default: 8)')
     args = parser.parse_args()
     
@@ -263,6 +279,7 @@ def main():
     
     # Update max_batches from command line argument
     evaluator.max_batches = args.max_batches
+    evaluator.batch_size = args.batch_size
     
     # Print evaluation configuration
     print(f"\nEvaluation Configuration:")
